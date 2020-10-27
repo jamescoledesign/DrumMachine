b829173fcb253ccda931379a74b5c0077bfc9e8e
James Cole <james@openstack.org>
2020-10-26 23:44:51 -0500

    Add keyboard controls

--- scripts.js
+++ scripts.js
@@ -6,5 +6,23 @@ let audioArr = [
 ];
 
 function activate(sound) {
+    if (!audioArr[sound]) {
+        alert("No sound assigned");
+    } else {
     audioArr[sound].play();
+    }
 }
+
+// Use keyboard to activate pad sound
+
+document.onkeydown = function(e) {
+    if (e.which == 81) {
+        activate(0);
+    } else if (e.which == 87) {
+        activate(1);w
+    } else if (e.which == 69) {
+        activate(2);
+    } else if (e.which == 82) {
+        activate(3);q
+    }
+  };
