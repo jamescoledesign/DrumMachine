0186833f86ebbc09e6ccad805efa99eb8c34af28
James Cole <james@openstack.org>
2020-10-26 22:23:58 -0500

    Modify functionality

--- scripts.js
+++ scripts.js
@@ -1,79 +1,10 @@
-function play1() {
-    var audio = document.getElementById("sound");
-    sound.play();
-}
+let audioArr = [
+    new Audio("sounds/Kick.wav"), 
+    new Audio("sounds/Hat-Closed.wav"), 
+    new Audio("sounds/Snare-Center.wav"),
+    new Audio("sounds/Snare-Rim.wav")
+];
 
-function play2() {
-    var audio = document.getElementById("sound2");
-    sound2.play();
-}
-
-function play3() {
-    var audio = document.getElementById("sound3");
-    sound3.play();
-}
-
-function play4() {
-    var audio = document.getElementById("sound3");
-    sound3.play();
-}
-
-function play5() {
-    var audio = document.getElementById("sound5");
-    sound5.play();
-}
-
-function play6() {
-    var audio = document.getElementById("sound6");
-    sound6.play();
-}
-
-function play7() {
-    var audio = document.getElementById("sound7");
-    sound7.play();
-}
-
-function play8() {
-    var audio = document.getElementById("sound8");
-    sound8.play();
-}
-
-function play9() {
-    var audio = document.getElementById("sound9");
-    sound9.play();
-}
-
-function play10() {
-    var audio = document.getElementById("sound10");
-    sound10.play();
-}
-
-function play11() {
-    var audio = document.getElementById("sound11");
-    sound11.play();
-}
-
-function play12() {
-    var audio = document.getElementById("sound12");
-    sound12.play();
-}
-
-function play13() {
-    var audio = document.getElementById("sound13");
-    sound13.play();
-}
-
-function play14() {
-    var audio = document.getElementById("sound14");
-    sound14.play();
-}
-
-function play15() {
-    var audio = document.getElementById("sound15");
-    sound15.play();
-}
-
-function play16() {
-    var audio = document.getElementById("sound16");
-    sound16.play();
+function activate(sound) {
+    audioArr[sound].play();
 }
