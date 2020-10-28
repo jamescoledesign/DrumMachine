58717413cc3cff2238dfba13422d02d556ad10bb
James Cole <james@openstack.org>
2020-10-28 16:46:14 -0500

    Clean up JS

--- scripts.js
+++ scripts.js
@@ -16,24 +16,60 @@ function pause(sound) {
     audioArr[sound].currentTime = 0;
 }
 
+const padOne = document.getElementById("pad1");
+const padTwo = document.getElementById("pad2");
+const padThree = document.getElementById("pad3");
+const padFour = document.getElementById("pad4");
+
+const padArr = [padOne, padTwo, padThree, padFour];
+
+let padSelection = 0;
+let sound = 0;
+let keyPressed = 0
+
+// Executes on each pad with mouse
+
+for (var i = 0; i < padArr.length; i++) {
+    (function(index) {
+        padArr[index].addEventListener("mousedown", function() {
+            padSelection = index;
+            console.log('Pad ' + padSelection + ' was clicked');
+            let sound = padSelection;
+            activate(sound);
+          })
+        // Pause audio with mouse 
+          padArr[index].addEventListener("mouseup", function() {
+           padSelection = index;
+           let sound = padSelection;
+           pause(sound);
+         })
+
+    })(i);
+}
+
 // Use keyboard to activate pad sound
 
 document.onkeydown = function(e) {
 
     let keyResult = e.key;
+    let padSelection = keyResult;
     
     if (keyResult == 1) {
         activate(0);
         document.getElementById("pad1").style.backgroundColor = "red";
+        console.log('Pad ' + padSelection + ' was typed');
     } else if (keyResult == 2) {
         activate(1);
         document.getElementById("pad2").style.backgroundColor = "red";
+        console.log('Pad ' + padSelection + ' was typed');
     } else if (keyResult == 3) {
         activate(2);
         document.getElementById("pad3").style.backgroundColor = "red";
+        console.log('Pad ' + padSelection + ' was typed');
     } else if (keyResult== 4) {
         activate(3);
         document.getElementById("pad4").style.backgroundColor = "red";
+        console.log('Pad ' + padSelection + ' was typed');
     }
   };
 
@@ -55,34 +91,3 @@ document.onkeydown = function(e) {
         document.getElementById("pad4").style.backgroundColor = "black";
     }
   };
-
-  // Executes on each pad
-
-let padOne = document.getElementById("pad1");
-let padTwo = document.getElementById("pad2");
-let padThree = document.getElementById("pad3");
-let padFour = document.getElementById("pad4");
-
-const padArr = [padOne, padTwo, padThree, padFour];
-let padSelection = 0;
-
-for (var i = 0; i < padArr.length; i++) {
-    (function(index) {
-        // Execute this code on each pad when clicked
-        //Play Audio
-         padArr[index].addEventListener("mousedown", function() {
-            padSelection = index;
-            console.log('Pad ' + padSelection + ' was clicked');
-            let sound = padSelection;
-            activate(sound);
-          })
-        // Pause Audio  
-          padArr[index].addEventListener("mouseup", function() {
-           padSelection = index;
-           let sound = padSelection;
-           pause(sound);
-         })
-    })(i);
-}
-
-
