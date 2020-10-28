8011e7bfba4e63dc2d488b3f8f2465180239d4dd
James Cole <james@openstack.org>
2020-10-28 00:01:12 -0500

    Change keycodes

--- scripts.js
+++ scripts.js
@@ -19,25 +19,31 @@ function pause(sound) {
 // Use keyboard to activate pad sound
 
 document.onkeydown = function(e) {
-    if (e.which == 49) {
+
+    let keyResult = e.key;
+    
+    if (keyResult == 1) {
         activate(0);
-    } else if (e.which == 50) {
+    } else if (keyResult == 2) {
         activate(1);
-    } else if (e.which == 51) {
+    } else if (keyResult == 3) {
         activate(2);
-    } else if (e.which == 52) {
+    } else if (keyResult== 4) {
         activate(3);
     }
   };
 
   document.onkeyup = function(e) {
-    if (e.which == 49) {
+
+    let keyResult = e.key;
+
+    if (keyResult == 1) {
         pause(0);
-    } else if (e.which == 50) {
+    } else if (keyResult == 2) {
         pause(1);
-    } else if (e.which == 51) {
+    } else if (keyResult == 3) {
         pause(2);
-    } else if (e.which == 52) {
+    } else if (keyResult== 4) {
         pause(3);
     }
   };
